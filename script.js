ebd4a3de75fc5dbc8e7e9799596c03a19d748f2c
Sirius523 <82580604+Sirius523@users.noreply.github.com>
2022-12-22 13:38:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+'use strict';
+// Пример 1
+let a1=7+2,
+a2=7-3,
+a3=2*4,
+a4=4/2;
+console.log(a1,a2,a3,a4)
+
+// Пример 2
+let height=23,
+width=10,
+S=width*height;
+console.log(S)
+
+// Пример 3
+let b1=5 % 3,
+b2=3 % 5,
+b3= 5 + '3',
+b4='5'-3,
+b5=75 + 'кг';
+console.log(b1,b2,b3,b4,b5);
+
+// Пример 4
+let heightC=10, 
+dC = 4,
+P=3.1415926535, 
+V= heightC*P*dC*dC/4;
+console.log(V);
+
+// Пример 5
+let a=10,
+b=2;
+console.log(a+b,a-b,a*b,a/b);
+
+// Пример 6
+let c=15,
+d=2,
+result= c+d;
+console.log(result);
+
+// Пример 7
+let a6=10,
+b7=2,
+c8=5;
+console.log(a6+b7+c8);
+ 
+// Пример 8
+let chislo= 17, 
+chislo1= 10,
+chislo2= a-b,
+chislo3= 7,
+result1= chislo2 + chislo3;
+console.log(result1);
+
+// Пример 9
+let name = "Кирилл"
+console.log('Привет,',name,'!');
+
+// Пример 10
+let age= "18"
+console.log("Мне", age, "лет",'!');
+
+// Пример 11
+let str = 'abcde'
+console.log(str[0], str[2], str[4]);
+
+// Пример 12
+let chas=60*60,
+syt= 24*chas,
+math= 30*syt;
+console.log(chas,syt,math);
+
+// Пример 13
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+console.log(`${num}`);
+
+// Пример 14
+let num1 = 10;
+num1++;
+num1++;
+num1--;
+console.log(`${num1}`);
+
+
+
